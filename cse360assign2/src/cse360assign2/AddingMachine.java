313679d4ad3f694504cae52ae3be29893f115c20
jcanez2 <56146625+jcanez2@users.noreply.github.com>
2019-10-03 23:40:57 -0700

    Added comments to the initial code provided.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -1,33 +1,87 @@
+/**
+ *  @author Jesus Canez 
+ *  @id	    CSE-360-85141
+ *  assignment2
+ */ 
+
 package cse360assign2;
+
+
+import java.lang.String;
+
+/**
+ * @name AddingMachine
+ * Class contains a constructor and various
+ * methods that perform various mathematical
+ * calculations in addition there is a clear method  
+ *
+ */
 
 public class AddingMachine {
 	private int	total;
+	
+	/**
+	 * constructor sets the initial value of the local 
+	 * variable total to zero
+	 */
 	
 	public AddingMachine()
 	{
 		total = 0; 	// not needed - included for clarity
 	}
 	
+	/**
+	 * getTotal returns the current value of the local
+	 * variable total
+	 * 
+	 * @return the current value of total
+	 */
+	
 	public int getTotal ()
 	{
 		return 0;
 	}
+	
+	/**
+	 * the add method takes in a value and adds it to
+	 * the current value of total
+	 * 
+	 * @param value an int to be added to total
+	 */
 	
 	public void add (int value)
 	{
 		
 	}
 	
+	/**
+	 * the subtract method takes in a value and subtracts
+	 * it from the local total variable 
+	 * 
+	 * @param value   an int to be subtracted from total
+	 */
+	
 	public void subtract (int value)
 	{
 		
 	}
+	
+	/**
+	 * toString method that currently returns an
+	 * empty string 
+	 *  
+	 * @return returns a string 
+	 */
+	
 	
 	public String toString ()
 	{
 		return "";
 	}
 	
+	/**
+	 * the clear method clears
+	 */
 	
 	public void clear ()
 	{
